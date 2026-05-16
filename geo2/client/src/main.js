c5027fefbc109c66679eb9bf9d3d4773ff310681
egors <egors@DESKTOP-LA93FBK>
2026-05-16 18:08:47 +0300

    Buildings, roads, poi все на месте

--- geo2/client/src/main.js
+++ geo2/client/src/main.js
@@ -1,41 +1,71 @@
 import './style.css';
-
 import 'ol/ol.css';
+
 import Map from 'ol/Map';
 import View from 'ol/View';
+
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 
 import ImageLayer from 'ol/layer/Image';
 import ImageWMS from 'ol/source/ImageWMS';
 
+import { fromLonLat } from 'ol/proj';
 
-const osmLayer = new TileLayer({
-  source: new OSM()
+const map = new Map({
+  target: 'map',
+
+  layers: [
+    new TileLayer({
+      source: new OSM()
+    })
+  ],
+
+  view: new View({
+    center: fromLonLat([49.2191, 53.5979]),
+    zoom: 17
+  })
 });
 
-
-const wmsLayer = new ImageLayer({
+const buildingsLayer = new ImageLayer({
   source: new ImageWMS({
     url: 'http://localhost:8080/geoserver/gis/wms',
+
     params: {
-      LAYERS: 'gis:buildings', // или gis:lab2_buildings — как у тебя назван слой
-      TILED: true
+      LAYERS: 'gis:buildings'
     },
+
     ratio: 1,
     serverType: 'geoserver'
   })
 });
 
+const roadsLayer = new ImageLayer({
+  source: new ImageWMS({
+    url: 'http://localhost:8080/geoserver/gis/wms',
 
-const map = new Map({
-  target: 'map',
-  layers: [
-    osmLayer,
-    wmsLayer
-  ],
-  view: new View({
-    center: [0, 0],
-    zoom: 2
+    params: {
+      LAYERS: 'gis:roads'
+    },
+
+    ratio: 1,
+    serverType: 'geoserver'
   })
 });
+
+const poiLayer = new ImageLayer({
+  source: new ImageWMS({
+    url: 'http://localhost:8080/geoserver/gis/wms',
+
+    params: {
+      LAYERS: 'gis:poi'
+    },
+
+    ratio: 1,
+    serverType: 'geoserver'
+  })
+});
+
+map.addLayer(buildingsLayer);
+map.addLayer(roadsLayer);
+map.addLayer(poiLayer);
